26b80efd3841c9f67970f22f3af874b7c9e012f3
jack bourke mckenna <jack bourke mckenna>
2017-01-18 19:29:38 +0000

    Array bug

---
+++ app/src/main/java/ie/jbmnetworks/apirequests/MainActivity.java
@@ -0,0 +1,89 @@
+package ie.jbmnetworks.apirequests;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    // Will show the string "data" that holds the results
+    TextView results;
+    // URL of object to be parsed
+    String JsonURL = "https://raw.githubusercontent.com/ianbar20/JSON-Volley-Tutorial/master/Example-JSON-Files/Example-Object.JSON";
+    // This string will hold the results
+    String data = "";
+    // Defining the Volley request queue that handles the URL request concurrently
+    RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Creates the Volley request queue
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Casts results into the TextView found within the main layout XML with id jsonData
+        results = (TextView) findViewById(R.id.jsonData);
+
+        // Creating the JsonObjectRequest class called obreq, passing required parameters:
+        //GET is used to fetch data from the server, JsonURL is the URL to be fetched from.
+        JsonObjectRequest obreq = new JsonObjectRequest(Request.Method.GET, JsonURL,
+                // The third parameter Listener overrides the method onResponse() and passes
+                //JSONObject as a parameter
+                new Response.Listener<JSONObject>() {
+
+                    // Takes the response from the JSON request
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+
+                            List<String> allNames = new ArrayList<String>();
+                            JSONArray author = response.getJSONArray("articles");
+                            for (int i=0; i<author.length(); i++) {
+                                JSONObject news = author.getJSONObject(i);
+                                String name = news.getString("author");
+
+                                data += "Author name " + name;
+                            }
+
+                            // Adds the data string to the TextView "results"
+                            results.setText(data);
+                        }
+                        // Try and catch are included to handle any errors due to JSON
+                        catch (JSONException e) {
+                            // If an error occurs, this prints the error to the log
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                // The final parameter overrides the method onErrorResponse() and passes VolleyError
+                //as a parameter
+                new Response.ErrorListener() {
+                    @Override
+                    // Handles errors that occur due to Volley
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley", "Error");
+                    }
+                }
+        );
+        // Adds the JSON object request "obreq" to the request queue
+        requestQueue.add(obreq);
+    }
+}
